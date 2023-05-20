64b09ed1ae1c5315d85b86b1f82d2eaefeea351b
Gr3ggg <134149181+Gr3ggg@users.noreply.github.com>
2023-05-20 22:18:29 +0200

    Update installzsh.sh

--- installzsh.sh
+++ installzsh.sh
@@ -125,7 +125,9 @@ alias upd='apt update'
 alias upg='apt upgrade -y'
 alias upa='apt update -y && apt upgrade -y && apt autoremove -y'
 
-alias wzsh='rm installzsh.sh && wget https://github.com/Gr3ggg/public/raw/main/installzsh.sh && chmod +x installzsh.sh'
+alias rwzsh='rm installzsh.sh && wget https://github.com/Gr3ggg/public/raw/main/installzsh.sh && chmod +x installzsh.sh'
+alias wzsh='wget https://github.com/Gr3ggg/public/raw/main/installzsh.sh && chmod +x installzsh.sh'
+
 
 EOF
 
