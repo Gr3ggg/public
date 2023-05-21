3d3462cfc56e8bdf6e9652204bc93e8c66821269
Gr3ggg <134149181+Gr3ggg@users.noreply.github.com>
2023-05-21 12:06:10 +0200

    Create funny_message.sh

---
+++ funny_message.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Définition d'une fonction pour afficher le message
+funny_msg() {
+  echo "Attention! Ce terminal est devenu autonome et cherche à communiquer avec les extraterrestres!"
+  echo "Veuillez ne pas paniquer et rester calme pendant que nous résolvons ce problème technologique inhabituel."
+  echo "Merci de votre coopération!"
+}
+
+# Appel de la fonction pour afficher le message drôle
+funny_msg
