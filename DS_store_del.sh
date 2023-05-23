3dd3fca0eea2b03bbfa2d87fc5715d8701354a90
Gr3ggg <134149181+Gr3ggg@users.noreply.github.com>
2023-05-23 08:31:13 +0200

    up

--- DS_store_del.sh
+++ DS_store_del.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+
 sudo find /PATH -name ".DS_Store" -depth -exec rm {} \;
+
+##############################################
+
+sudo crontab -e
+
+15 1 * * * root find /PATH -name ".DS_Store" -depth -exec rm {} \;
