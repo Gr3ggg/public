4bcae7fa37d683e56a1ec4193013d707c0d10067
greg <134149181+Gr3ggg@users.noreply.github.com>
2023-05-29 19:10:00 +0200

    up

--- unbound.sh
+++ unbound.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# copier/coller ds votre Shell: rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh
+alias wunbound='rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh'
+
 # Mise à jour du système
 apt update
 apt upgrade -y
@@ -20,8 +23,16 @@ server:
   do-udp: yes
   do-tcp: yes
   access-control: 10.0.0.0/8 allow
+  access-control: 2a01:e0a:477:9df0::1/64 allow
+  access-control: 2a01:e0a:477:9df1::1/64 allow
+  access-control: 2a01:e0a:477:9df2::1/64 allow
+  access-control: 2a01:e0a:477:9df3::1/64 allow  
+  access-control: 2a01:e0a:477:9df4::1/64 allow  
+  access-control: 2a01:e0a:477:9df5::1/64 allow  
+  access-control: 2a01:e0a:477:9df6::1/64 allow  
+  access-control: 2a01:e0a:477:9df7::1/64 allow
   access-control: 0.0.0.0/0 refuse
-  access-control: ::/0 allow
+  access-control: ::/0 refuse
   do-daemonize: yes
   verbosity: 3
   hide-identity: yes
@@ -47,24 +58,23 @@ server:
   unwanted-reply-threshold: 10000
   do-not-query-localhost: yes
   val-clean-additional: yes
-  logfile: /var/log/unbound/unbound.log
+  logfile: "/var/log/unbound/unbound.log"
   log-queries: yes
   log-time-ascii: yes
 
-  private-domain: "example.com"                      ## a modifier
-  local-zone: "example.com." static                  ## a modifier
-  local-data: "example.com. IN A 192.168.1.100"      ## a modifier
-  local-data-ptr: "192.168.1.100 example.com"        ## a modifier
-
+  #private-domain: "example.com"                      ## a modifier
+  #local-zone: "example.com." static                  ## a modifier
+  #local-data: "example.com. IN A 192.168.1.100"      ## a modifier
+  #local-data-ptr: "192.168.1.100 example.com"        ## a modifier
 
 root-hints: "/var/lib/unbound/root.hints"
 
-forward-zone:
-  name: "."
-  forward-addr: 1.1.1.1
-  forward-addr: 1.0.0.1
-  forward-addr: 2606:4700:4700::1111
-  forward-addr: 2606:4700:4700::1001
+#forward-zone:
+#  name: "."
+#  forward-addr: 1.1.1.1
+#  forward-addr: 1.0.0.1
+#  forward-addr: 2606:4700:4700::1111
+#  forward-addr: 2606:4700:4700::1001
 
 ##je bloque cetaines pubs
 local-zone: "doubleclick.net" redirect
@@ -90,6 +100,26 @@ wget -O /var/lib/unbound/root.hints https://www.internic.net/domain/named.cache
 # Définition des permissions sur le fichier de configuration et les serveurs root
 chown unbound:unbound /etc/unbound/unbound.conf
 chown unbound:unbound /var/lib/unbound/root.hints
+mkdir /var/log/unbound
+chown unbound:unbound /var/log/unbound
+
+# Récupérer l'adresse IP de la machine
+IP_machine=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# Vérifier si l'adresse IP est vide
+if [ -z "$IP_machine" ]; then
+  echo "Impossible de récupérer l'adresse IP de la machine."
+  exit 1
+fi
+
+# Modifier le fichier /etc/resolv.conf
+sed -i "1s/.*/domain server/" /etc/resolv.conf
+sed -i "2s/.*/search server/" /etc/resolv.conf
+sed -i "3s/.*/nameserver $IP_machine/" /etc/resolv.conf
+
+# Ajouter les autres serveurs DNS à la fin du fichier
+echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+echo "#nameserver 1.0.0.1" >> /etc/resolv.conf
 
 # Démarrage du service Unbound
 systemctl start unbound
