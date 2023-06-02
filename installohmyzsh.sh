1c4f0e5bdd7e253efb113351d5397fd59e9ee5d4
greg <134149181+Gr3ggg@users.noreply.github.com>
2023-06-02 22:54:53 +0200

    up

--- installohmyzsh.sh
+++ installohmyzsh.sh
@@ -139,7 +139,7 @@ prompt_ipclient() {
                   # en 1 ligne
 
                     if [[ $EUID -eq 0 ]]; then
-                      PROMPT=$'%F{cyan}${debian_chroot:+($debian_chroot)──}(%B%(#.%F{red}%n.%F{green}%n)%F{green}@%F{blue}%m%b%F{cyan}) %F{cyan}[%B%F{yellow}%(6~.%-1~/…/%4~.%5~)%b%F{cyan}] %F{cyan}-%F{red}▶%b%F{reset}'
+                      PROMPT=$'%F{cyan}${debian_chroot:+($debian_chroot)──}(%B%(#.%F{red}%n.%F{green}%n)%F{green}@%F{blue}%m%b%F{cyan}) -[%F{red}$(prompt_ipmachine)%F{cyan}]- %F{cyan}[%B%F{yellow}%(6~.%-1~/…/%4~.%5~)%b%F{cyan}] %F{cyan}-%F{red}▶%b%F{reset}'
                     else
                       PROMPT=$'%F{cyan}${debian_chroot:+($debian_chroot)──}(%B%(#.%F{red}%n.%F{green}%n)%F{green}@%F{blue}%m%b%F{cyan}) %F{cyan}[%B%F{yellow}%(6~.%-1~/…/%4~.%5~)%b%F{cyan}] %F{cyan}-%F{green}▶%b%F{reset}'
                     fi
