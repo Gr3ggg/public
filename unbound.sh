18d0f1fd7a31e82cc6390d565215a5faf3908941
greg <134149181+Gr3ggg@users.noreply.github.com>
2023-06-18 18:38:03 +0200

    up

--- unbound.sh
+++ unbound.sh
@@ -8,14 +8,14 @@ shell=$(basename "$SHELL")
 # Vérifier le shell et sourcer le fichier de configuration approprié
 if [ "$shell" = "bash" ]; then
     # Vérifier si la ligne existe déjà dans .bashrc
-    if ! grep -q "alias wunbound='cd && rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh'" ~/.bashrc; then
-        echo "alias wunbound='cd && rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh'" >> ~/.bashrc
+    if ! grep -q "alias wwunbound='cd && rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh'" ~/.bashrc; then
+        echo "alias wwunbound='cd && rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh'" >> ~/.bashrc
     fi
     source ~/.bashrc
 elif [ "$shell" = "zsh" ]; then
     # Vérifier si la ligne existe déjà dans .zshrc
-    if ! grep -q "alias wunbound='cd && rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh'" ~/.zshrc; then
-        echo "alias wunbound='cd && rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh'" >> ~/.zshrc
+    if ! grep -q "alias wwunbound='cd && rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh'" ~/.zshrc; then
+        echo "alias wwunbound='cd && rm -f unbound.sh && wget https://github.com/Gr3ggg/public/raw/main/unbound.sh && chmod +x unbound.sh && ./unbound.sh'" >> ~/.zshrc
     fi
     source ~/.zshrc
 fi
