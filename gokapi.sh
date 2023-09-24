46cf5a2712d0b968287475759d08c4eff40ada8e
Greg <134149181+Gr3ggg@users.noreply.github.com>
2023-09-24 20:31:43 +0200

    up

--- gokapi.sh
+++ gokapi.sh
@@ -6,7 +6,6 @@ if ! command -v docker &> /dev/null; then
     echo "Docker n'est pas installé. Veuillez installer Docker avant de continuer."
     exit 1
 fi
-
 # Vérifier si le réseau "npmproxy" existe
 if ! docker network inspect npmproxy &> /dev/null; then
     echo "Le réseau 'npmproxy' n'existe pas. Assurez-vous de le créer."
